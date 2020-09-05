f03d4e7e1d16820a37517c4b916ace29320cef9f
= <=>
2020-09-05 09:54:52 -0300

    action on click in logo to reset selectedCharacter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,24 @@ import React from 'react';
 import './assets/header.css';
 import logo from './assets/logo.png';
 import avatar from './assets/avatar.jpeg';
+import { Consumer } from '../StateManager';
 
 export const Header = () => {
   return (
     <header className="d-flex justify-content-between p-1">
-      <img className="logo" alt="objective logo" src={logo} />
+      <Consumer>
+        {({ setSelectedCharacter }) => {
+          return (
+            <img
+              src={logo}
+              alt="objective logo"
+              className="logo cursor-pointer"
+              onClick={() => setSelectedCharacter(null)}
+            />
+          );
+        }}
+      </Consumer>
+
       <div className="d-flex">
         <div className="candidate-info">
           <b className="candidate-name mr-1">Rafael Umbelino</b>
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { Header } from './Header';
+import { Provider } from '../StateManager';
 
 describe('Header', () => {
-  const wrapper = shallow(<Header />);
+  const value = {
+    setSelectedCharacter: jest.fn(),
+  };
+  const wrapper = mount(
+    <Provider value={value}>
+      <Header />
+    </Provider>,
+  );
 
   it('should render the logo', () => {
     const { src, alt } = wrapper.find('.logo').props();
     expect(src).toBe('logo.png');
     expect(alt).toBe('objective logo');
+  });
+
+  it('should call setSelecterCharacter with null on click in logo', () => {
+    wrapper.find('.logo').simulate('click');
+    expect(value.setSelectedCharacter).toHaveBeenCalledWith(null);
   });
 
   it('should render candidate name', () => {
