a1159981abbcd1017151856f4c1981664f437d77
= <=>
2020-09-05 09:57:24 -0300

    removing exception handler on fetchEvents

--- src/components/Detail/fetchEvent.js
+++ src/components/Detail/fetchEvent.js
@@ -1,15 +1,11 @@
 import axios from '../../utils/axios';
 
 export const fetchEvent = async (resourceURI) => {
-  try {
-    const response = await axios.get(resourceURI);
+  const response = await axios.get(resourceURI);
 
-    const [event] = response.data.data.results;
-    const { title, thumbnail, description } = event;
-    const { path, extension } = thumbnail;
+  const [event] = response.data.data.results;
+  const { title, thumbnail, description } = event;
+  const { path, extension } = thumbnail;
 
-    return { title, thumbnail: `${path}/portrait_small.${extension}`, description };
-  } catch (error) {
-    console.log(error);
-  }
+  return { title, thumbnail: `${path}/portrait_small.${extension}`, description };
 };
