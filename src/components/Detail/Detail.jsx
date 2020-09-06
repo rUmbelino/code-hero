7a8d301b2d0abe8f7b9d782534992330964610cf
= <=>
2020-09-05 22:32:43 -0300

    insertion of detail description as HTML

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -11,32 +11,25 @@ export const Detail = ({ thumbnail, name, description, events }) => {
   const title = events.items.length ? EVENTS_TITLE : '';
 
   return (
-    <div className='container '>
-      <article className='card rounded'>
+    <div className="container ">
+      <article className="card rounded">
         <Consumer>
           {({ setSelectedCharacter }) => (
-            <button
-              className='my-1 cursor-pointer'
-              onClick={() => setSelectedCharacter(null)}
-            >
+            <button className="my-1 cursor-pointer" onClick={() => setSelectedCharacter(null)}>
               &#8592; voltar
             </button>
           )}
         </Consumer>
 
-        <div className='d-flex'>
-          <img
-            alt={name}
-            className='rounded'
-            src={`${path}/portrait_xlarge.${extension}`}
-          />
-          <div className='ml-1'>
-            <h2 className='name py-1'>{name}</h2>
-            <p className='description'>{description}</p>
+        <div className="d-flex">
+          <img alt={name} className="rounded" src={`${path}/portrait_xlarge.${extension}`} />
+          <div className="ml-1">
+            <h2 className="name py-1">{name}</h2>
+            <p className="description" dangerouslySetInnerHTML={{ __html: description }}></p>
           </div>
         </div>
-        <section className='d-flex flex-column justify-content-center my-1'>
-          <h3 className='align-self-start  my-1'>{title}</h3>
+        <section className="d-flex flex-column justify-content-center my-1">
+          <h3 className="align-self-start  my-1">{title}</h3>
           {events.items.map(({ name, resourceURI }, index) => (
             <Event key={`${name}_${index}`} resourceURI={resourceURI} />
           ))}
